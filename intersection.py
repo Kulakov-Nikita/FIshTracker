9c22fde3bbda2bacdff3d24ac92e170d506d795a
Nikita_Kulakov <kulakofnikita@gmail.com>
2024-10-18 15:13:11 +0300

    Added Intersection Detector

--- intersection.py
+++ intersection.py
@@ -1,8 +1,43 @@
 import torch
+from matplotlib import pyplot as plt
+
+
+class IntersectionDetector:
+    def __init__(self, center_pos: tuple[int, int], center_sector_radius: float, middle_sector_radius: float,
+                 outer_sector_radius: float, periphery_radius: float, image_shape: tuple[int, int],
+                 threshold: float) -> None:
+        self.center_sector: Sector = Sector(center_pos, center_sector_radius, image_shape)
+        self.middle_sector: Sector = Sector(center_pos, middle_sector_radius, image_shape)
+        self.outer_sector: Sector = Sector(center_pos, outer_sector_radius, image_shape)
+        self.periphery: Sector = Sector(center_pos, periphery_radius, image_shape)
+
+        self.center_sector_frame_num: int = 0
+        self.middle_sector_frame_num: int = 0
+        self.outer_sector_frame_num: int = 0
+        self.periphery_sector_frame_num: int = 0
+
+        self.threshold: float = threshold
+
+    def intersection(self, frames) -> None:
+        in_center_fish_area = self.center_sector.intersection(frames)
+        in_middle_fish_area = self.middle_sector.intersection(frames)
+        in_outer_fish_area = self.outer_sector.intersection(frames)
+        on_periphery_fish_area = self.periphery.intersection(frames)
+
+        self.periphery_sector_frame_num += torch.sum((on_periphery_fish_area - in_outer_fish_area > self.threshold))
+        self.outer_sector_frame_num += torch.sum((in_outer_fish_area - in_middle_fish_area > self.threshold))
+        self.middle_sector_frame_num += torch.sum((in_middle_fish_area - in_center_fish_area > self.threshold))
+        self.center_sector_frame_num += torch.sum(in_center_fish_area > self.threshold)
+
+    def draw_plot(self, frames) -> None:
+        intersection = self.middle_sector.intersection(frames) - self.center_sector.intersection(frames)
+        intersection = intersection.cpu().detach().numpy()
+        plt.plot(range(len(intersection)), intersection)
+        plt.show()
 
 
 class Sector:
-    def __init__(self, center_pos: tuple[int, int], radius: float, image_shape: tuple[int, int]):
+    def __init__(self, center_pos: tuple[int, int], radius: float, image_shape: tuple[int, int]) -> None:
         # Создаём координаты для каждого пикселя в тензоре
         x = torch.arange(image_shape[0], dtype=torch.float32)
         y = torch.arange(image_shape[1], dtype=torch.float32)
@@ -14,7 +49,7 @@ class Sector:
         # Создаём маску, где внутри окружности значения 1, а снаружи 0
         self.mask = (distance <= radius).float().to('cuda')
 
-    def intersection(self, image):
-        intersection = self.mask * image
-        return torch.sum(intersection, dim=(1, 2), keepdim=True)
+    def intersection(self, frames):
+        intersection = self.mask * frames
+        return torch.sum(intersection, dim=(2, 3), keepdim=True).reshape(len(frames))
 
--- main.py
+++ main.py
@@ -10,21 +10,34 @@ def main():
     model = torch.load("models/torch_eight.pth").to('cuda')
 
     # Загрузка пакета
-    video_np: np.ndarray = read_video(path_to_input_video='input/fenibut_2.mp4', video_duration_sec=3)
+    video_np: np.ndarray = read_video(path_to_input_video='input/fenibut_2.mp4', video_duration_sec=5)
 
-    circle_center = (610, 400)
-    circle_radius = 125
+    sector_center = (610, 400)
+    center_sector_radius = 45
+    middle_sector_radius = 125
+    outer_sector_radius = 225
+    periphery_radius = 280
+
+    intersection_detector = IntersectionDetector(sector_center, center_sector_radius, middle_sector_radius,
+                                                 outer_sector_radius, periphery_radius, video_np.shape[1:],
+                                                 threshold=0)
 
     # Отправка пакета на GPU
-    video_torch = torch.tensor(video_np, dtype=torch.uint8, device='cuda')
+    video_torch = torch.tensor(video_np[60:], dtype=torch.uint8, device='cuda')
     video_torch = video_torch.to(torch.float32)
 
     # Обработка пакета
     video_torch = video_torch.permute(0, 3, 1, 2)
     result = model(video_torch)
 
-    sector = Sector(circle_center, circle_radius, result.shape[2:])
-    result = sector.mask * result
+    intersection_detector.intersection(result)
+
+    print(f"Время в центральном секторе: {intersection_detector.center_sector_frame_num / 30 :.1f}s")
+    print(f"Время в среднем секторе: {intersection_detector.middle_sector_frame_num / 30 :.1f}s")
+    print(f"Время во внешнем секторе секторе: {intersection_detector.outer_sector_frame_num / 30 :.1f}s")
+    print(f"Время на перферии: {intersection_detector.periphery_sector_frame_num / 30 :.1f}s")
+
+    #result = intersection_detector.outer_sector.mask + result
 
     # Возврат пакета на CPU
     result = result.to(torch.uint8)
@@ -36,7 +49,7 @@ def main():
     # result = np.stack((result,)*3, axis=3).reshape(result.shape[0], result.shape[1], result.shape[2], 3)
 
     # Обводим область с рыбкой
-    result = draw_contours(background_frames=video_np, mask_frames=result)
+    result = draw_contours(background_frames=video_np[60:], mask_frames=result)
 
     # Запись результата на диск
     write_video(frames=result, path_to_output_video='output/fenibut_2.avi')
