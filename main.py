81339836266101b677cbe08026fd043ab3d7aeb7
Nikita_Kulakov <kulakofnikita@gmail.com>
2024-10-26 10:54:53 +0300

    Added video processing in batches

--- main.py
+++ main.py
@@ -6,40 +6,7 @@ from movement import MovementDetector
 from matplotlib import pyplot as plt
 
 
-def main():
-    # Настройки
-    sector_center = (610, 400)
-    center_sector_radius = 45
-    middle_sector_radius = 125
-    outer_sector_radius = 225
-    periphery_radius = 280
-    batch_size = 90
-    pixels_per_centimeter = 1
-
-    # Загрузка модели на GPU
-    model = torch.load("models/torch_eight.pth").to('cuda')
-
-    # Загрузка пакета
-    video_np: np.ndarray = read_video(path_to_input_video='input/fenibut_2.mp4', video_duration_sec=6)
-
-    image_shape = video_np.shape[1:-1]
-
-    intersection_detector = IntersectionDetector(sector_center, center_sector_radius, middle_sector_radius,
-                                                 outer_sector_radius, periphery_radius, image_shape,
-                                                 threshold=0)
-
-    movement_detector = MovementDetector(zone=intersection_detector.periphery, image_shape=image_shape,
-                                         batch_size=batch_size, pixels_per_centimeter=pixels_per_centimeter,
-                                         frames_num=batch_size)
-
-    # Отправка пакета на GPU
-    video_torch = torch.tensor(video_np[90:], dtype=torch.uint8, device='cuda')
-    video_torch = video_torch.to(torch.float32)
-
-    # Обработка пакета
-    video_torch = video_torch.permute(0, 3, 1, 2)
-    result = model(video_torch)
-
+def print_info(intersection_detector, movement_detector, result):
     print("Пакет обработан")
 
     intersection_detector.intersection(result)
@@ -54,22 +21,83 @@ def main():
     print(f"Длина пройденная рыбкой: {movement_detector.distance():.1f}cm")
     print(f"Время без движения: {movement_detector.time_without_movement(2) / 30:.1f}s")
 
-    #result = intersection_detector.outer_sector.mask + result
 
-    # Возврат пакета на CPU
-    result = result.to(torch.uint8)
-    result = result.cpu()
+def print_info_end(intersection_detector, movement_detector):
+    print(f"Время в центральном секторе: {intersection_detector.center_sector_frame_num / 30 :.1f}s")
+    print(f"Время в среднем секторе: {intersection_detector.middle_sector_frame_num / 30 :.1f}s")
+    print(f"Время во внешнем секторе секторе: {intersection_detector.outer_sector_frame_num / 30 :.1f}s")
+    print(f"Время на перферии: {intersection_detector.periphery_sector_frame_num / 30 :.1f}s")
 
-    # Преобразуем tensor в numpy
-    result = result.permute(0, 2, 3, 1)
-    result = result.numpy()
-    # result = np.stack((result,)*3, axis=3).reshape(result.shape[0], result.shape[1], result.shape[2], 3)
+    print(f"Длина пройденная рыбкой: {movement_detector.distance():.1f}cm")
+    print(f"Время без движения: {movement_detector.time_without_movement(2) / 30:.1f}s")
 
-    # Обводим область с рыбкой
-    result = draw_contours(background_frames=video_np[60:], mask_frames=result)
+def main():
+    # Настройки
+    sector_center = (610, 400)
+    center_sector_radius = 45
+    middle_sector_radius = 125
+    outer_sector_radius = 225
+    periphery_radius = 280
+    image_shape = (720, 1280)
+    batch_size = 90
+    pixels_per_centimeter = 1
 
-    # Запись результата на диск
-    write_video(frames=result, path_to_output_video='output/fenibut_2.avi')
+    # Загрузка модели на GPU
+    model = torch.load("models/torch_eight.pth").to('cuda')
+    model.eval()
+
+    intersection_detector = IntersectionDetector(sector_center, center_sector_radius, middle_sector_radius,
+                                                 outer_sector_radius, periphery_radius, image_shape,
+                                                 threshold=0)
+
+    movement_detector = MovementDetector(zone=intersection_detector.periphery, image_shape=image_shape,
+                                         batch_size=batch_size, pixels_per_centimeter=pixels_per_centimeter,
+                                         frames_num=batch_size)
+
+    video_reader = VideoReader(path_to_input_video='input/fenibut_2.mp4', image_shape=image_shape, batch_size=batch_size)
+
+    counter = 0
+
+    for batch in video_reader.read_batch():
+        batch_torch = torch.tensor(batch, device="cuda:0").to(torch.float32).permute(0, 3, 1, 2)
+        with torch.no_grad():
+            result = model(batch_torch)
+
+        intersection_detector.intersection(result)
+        movement_detector.compute_distances(result)
+
+        del result
+        del batch_torch
+        torch.cuda.empty_cache()
+        print(counter)
+        counter += 1
+
+    print_info_end(intersection_detector, movement_detector)
+
+    # # Загрузка пакета
+    # video_np: np.ndarray = read_video(path_to_input_video='input/fenibut_2.mp4', video_duration_sec=9)
+    #
+    # # Отправка пакета на GPU
+    # video_torch = torch.tensor(video_np[90:180], dtype=torch.uint8, device='cuda')
+    # video_torch = video_torch.to(torch.float32)
+    #
+    # # Обработка пакета
+    # video_torch = video_torch.permute(0, 3, 1, 2)
+    #
+    # with torch.no_grad():
+    #     result = model(video_torch)
+    #
+    # #print_info(intersection_detector, movement_detector, result)
+    # print('done')
+    # del video_torch
+    # del result
+    # torch.cuda.empty_cache()
+    # input()
+
+    # video_torch = torch.tensor(video_np[180:270], dtype=torch.uint8, device='cuda')
+    # video_torch = video_torch.to(torch.float32)
+
+
 
 
 if __name__ == '__main__':
--- video_processing.py
+++ video_processing.py
@@ -1,5 +1,38 @@
 import cv2
 import numpy as np
+import torch
+
+
+class VideoReader:
+    def __init__(self, path_to_input_video: str, image_shape: tuple[int, int], batch_size: int):
+        self.path_to_input_video = path_to_input_video
+        self.image_shape: tuple[int, int] = image_shape
+        self.batch_size: int = batch_size
+
+        self.video_capture = cv2.VideoCapture(self.path_to_input_video)
+
+        if not self.video_capture.isOpened():
+            raise Exception("Error opening video file: " + path_to_input_video)
+
+    def read_batch(self) -> torch.Tensor:
+        frames = np.zeros((self.batch_size, self.image_shape[0], self.image_shape[1], 3), dtype=np.int8)
+        counter: int = 0
+        while self.video_capture.isOpened() and counter < self.batch_size:
+            ret, frame = self.video_capture.read()
+            if ret:
+                frames[counter] = frame
+                counter += 1
+            else:
+                self.release()
+
+            if counter == self.batch_size:
+                yield frames
+                counter = 0
+
+    def release(self):
+        if self.video_capture is not None:
+            self.video_capture.release()
+
 
 
 def read_video(path_to_input_video: str, video_resolution: tuple = (1280, 720), video_duration_sec: int = 300,
@@ -16,19 +49,19 @@ def read_video(path_to_input_video: str, video_resolution: tuple = (1280, 720), 
         if ret:
             frames[counter] = frame
             counter += 1
-            # if counter % (video_duration_frames // 100) == 0:
-            #     print(f"{counter // (video_duration_frames // 100)}%")
+            if counter % (video_duration_frames // 100) == 0:
+                print(f"{counter // (video_duration_frames // 100)}%")
         else:
             cap.release()
 
     return frames
 
 
-def write_video(frames: np.ndarray, path_to_output_video: str, fps: int = 30) -> None:
-    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-    out = cv2.VideoWriter(path_to_output_video, fourcc, fps, (frames.shape[2], frames.shape[1]))
-    print(frames.shape, frames.dtype)
-    for frame in frames:
-        out.write(frame)
-    out.release()
+# def write_video(frames: np.ndarray, path_to_output_video: str, fps: int = 30) -> None:
+#     fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+#     out = cv2.VideoWriter(path_to_output_video, fourcc, fps, (frames.shape[2], frames.shape[1]))
+#     print(frames.shape, frames.dtype)
+#     for frame in frames:
+#         out.write(frame)
+#     out.release()
 
